c5a1345060579a2987b781b237b9ffc494e5293d
MorenoDamiaensPXL <moreno.damiaens@student.pxl.be>
2025-11-02 23:41:47 +0100

    test

--- oplossing/variable.tf
+++ oplossing/variable.tf
@@ -1,50 +1,33 @@
 variable "my_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
+    description = "AWS region"
+    type = string
+    default = "us-east-1"
 }
 
 variable "my_instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.small"
+    description = "my instance type"
+    type = string
+    default = "t3.micro"
 }
 
 variable "key_name" {
-  description = "EC2 keypair name"
-  type        = string
-}
+    description = "my keypair name for the EC2"
+    type = string
 
+}
+// For SSH TEsting  
 variable "allowed_client_cidr" {
-  description = "SSH source CIDR"
-  type        = string
-  default     = "84.195.212.134/32"
+    description = "my client ip address"
+    type = string
+    default = "84.195.212.134/32"
 }
 
+variable "userdata_file" {
+  description = "The User data file thats needed for the golang app "
+  default = "userdata.sh"
+}
 variable "ami" {
-  description = "Ubuntu AMI (region-specific)"
-  type        = string
-  default     = "ami-0360c520857e3138f"
-}
-
-# Immutability inputs from CI
-variable "docker_ns" {
-  description = "Docker Hub namespace (will be lowercased in user-data)"
-  type        = string
-}
-
-variable "backend_digest" {
-  description = "Digest for backend image (sha256:...)"
-  type        = string
-}
-
-variable "frontend_digest" {
-  description = "Digest for frontend image (sha256:...)"
-  type        = string
-}
-
-variable "mongo_digest" {
-  description = "Digest for Mongo image (sha256:...). If empty, falls back to a pinned tag."
-  type        = string
-  default     = ""
+    description = "ubuntu ami"
+    type = string
+    default = "ami-0360c520857e3138f"
 }
