61fffd2f6f6f56d4c4a8648039f2861721d0e060
MorenoDamiaensPXL <moreno.damiaens@student.pxl.be>
2025-11-02 23:57:12 +0100

    final?

--- oplossing/outputs.tf
+++ oplossing/outputs.tf
@@ -1,17 +1,22 @@
+############################################
+# Outputs
+############################################
+output "api_gateway_url" {
+  value       = aws_apigatewayv2_api.todo_api.api_endpoint
+  description = "Invoke URL van de HTTP API"
+}
 
-
-# --- Route Table ID ---
 output "public_route_table_id" {
-  description = "ID of the public route table"
+  description = "ID van de public route table"
   value       = aws_route_table.public_rt.id
 }
 
 output "internet_gateway_id" {
-  value = aws_internet_gateway.igw.id 
-  }
-
-  output "static_ip" {
-  description = "Static public IP of the EC2 instance"
-  value       = aws_instance.frontend.public_ip
+  value       = aws_internet_gateway.igw.id
+  description = "ID van de Internet Gateway"
 }
 
+output "static_ip" {
+  description = "Static public IP van de frontend EC2 (EIP)"
+  value       = aws_eip.frontend_eip.public_ip
+}
