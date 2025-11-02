dc6360586c456d620e61d5c26049a57cc639f2a3
MorenoDamiaensPXL <moreno.damiaens@student.pxl.be>
2025-11-03 00:00:28 +0100

    final????

--- oplossing/main.tf
+++ oplossing/main.tf
@@ -170,25 +170,3 @@ resource "aws_apigatewayv2_stage" "todo_stage" {
   auto_deploy = true
 }
 
-############################################
-# Outputs
-############################################
-output "api_gateway_url" {
-  value       = aws_apigatewayv2_api.todo_api.api_endpoint
-  description = "Invoke URL van de HTTP API"
-}
-
-output "public_route_table_id" {
-  description = "ID van de public route table"
-  value       = aws_route_table.public_rt.id
-}
-
-output "internet_gateway_id" {
-  value       = aws_internet_gateway.igw.id
-  description = "ID van de Internet Gateway"
-}
-
-output "static_ip" {
-  description = "Static public IP van de frontend EC2 (EIP)"
-  value       = aws_eip.frontend_eip.public_ip
-}
